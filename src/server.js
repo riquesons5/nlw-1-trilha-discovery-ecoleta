a24368154094a4d43670cc4ab7dfb423ef3eeeef
Henrique Franco <henrique.franco11@yahoo.com.br>
2020-10-12 21:50:44 -0300

    template engine config

--- src/server.js
+++ src/server.js
@@ -4,20 +4,27 @@ const server = express()
 //configurando pasta public
 server.use(express.static("public"))
 
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos da minha aplicação
 //pagina inciial
 //req: Requisição
 //res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Seu marketplace de coleta de resíduos" })
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 //ligar o servidor
